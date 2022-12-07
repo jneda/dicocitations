7f1b880c7e78434ed72d053d76eecb5dc754d424
jdane <julien.dane@gmail.com>
2022-12-07 10:09:06 +0100

    Refactored into functions

--- index.php
+++ index.php
@@ -2,61 +2,70 @@
 
 include './templates/header.html';
 
-// establish connection
-$connection = null;
-try {
-  // get database config
-  $dbConfigFile = file_get_contents('./config/config.json');
-  $dbConfig = json_decode($dbConfigFile);
-  //var_dump(get_object_vars($dbConfig->database));
-  extract(get_object_vars($dbConfig->database));
+function getConnection()
+{
+  // establish connection
+  try {
+    // get database config
+    $dbConfigFile = file_get_contents('./config/config.json');
+    $dbConfig = json_decode($dbConfigFile);
 
-  $connection = new PDO(
-    'mysql:host=' . $host . ';dbname=' . $dbname,
-    $login,
-    $password
-  );
-  $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //echo 'Connection established';
-} catch (Exception $e) {
-  die('Connection failed: ' . $e->getMessage());
+    extract(get_object_vars($dbConfig->database));
+
+    $connection = new PDO(
+      'mysql:host=' . $host . ';dbname=' . $dbname,
+      $login,
+      $password
+    );
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    return $connection;
+  } catch (Exception $e) {
+    die('Connection failed: ' . $e->getMessage());
+  }
 }
 
-// get a random quote
+function getRandomQuote($connection)
+{
+  // get a random quote
 
-// get quotes count
-$statement = $connection->prepare('
-  SELECT * FROM quote
-  INNER JOIN author ON author.id = quote.authorId
-');
-$statement->execute();
-$result = $statement->fetchAll();
+  // get quotes count
+  $statement = $connection->prepare('
+    SELECT * FROM quote
+    INNER JOIN author ON author.id = quote.authorId
+  ');
+  $statement->execute();
+  $result = $statement->fetchAll();
 
-$randomQuote = $result[rand(0, count($result) - 1)];
+  return $result[rand(0, count($result) - 1)];
+}
 
-include('./templates/quote.php');
+function getAuthors($connection)
+{
+  // get all distinct author names
+  $statement = $connection->prepare('
+    SELECT DISTINCT id, lastName, firstName FROM author ORDER BY lastName
+  ');
+  $statement->execute();
+  return $statement->fetchAll();
+}
 
-// get all distinct author names
-$statement = $connection->prepare('
-  SELECT DISTINCT id, lastName, firstName FROM author ORDER BY lastName
-');
-$statement->execute();
-$authors = $statement->fetchAll();
+function getCenturies($connection)
+{
+  // get all distinct centuries
+  $statement = $connection->prepare('
+    SELECT DISTINCT century FROM author ORDER BY century
+  ');
+  $statement->execute();
+  return $statement->fetchAll();
+}
 
-//var_dump($authors);
-
-// get all distinct centuries
-$statement = $connection->prepare('
-  SELECT DISTINCT century FROM author ORDER BY century
-');
-$statement->execute();
-$centuries = $statement->fetchAll();
-
-//var_dump($centuries);
-
-// close connection
+$connection = getConnection();
+$randomQuote = getRandomQuote($connection);
+$authors = getAuthors($connection);
+$centuries = getCenturies($connection);
 $connection = null;
 
+include('./templates/quote.php');
 include './templates/searchQuoteForm.php';
-
 include './templates/footer.html';
