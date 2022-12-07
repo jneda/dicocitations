f7109f4f46b643f366e5a1ca298ad4d4ab8746d9
jdane <julien.dane@gmail.com>
2022-12-07 09:06:57 +0100

    More Bootstrap styling

--- includes/searchQuoteForm.php
+++ includes/searchQuoteForm.php
@@ -4,43 +4,47 @@
       <label for="q" class="form-label">Rechercher :</label>
       <input type="search" name="q" id="q" class="form-control">
     </div>
-    <div class="mb-3">
-      <label for="author" class="form-label">Auteur :</label>
-      <select name="author" id="author">
-        <option disabled selected value></option>
-        <?php foreach ($authors as $author) { ?>
-          <option value="<?= $author['id'] ?>">
-            <?= $author['lastName'] . ' ' . $author['firstName'] ?>
-          </option>
-        <?php } ?>
-      </select>
+    <div class="row">
+      <div class="mb-3 col">
+        <label for="author" class="form-label">Auteur :</label>
+        <select name="author" id="author" class="form-select">
+          <option selected value></option>
+          <?php foreach ($authors as $author) { ?>
+            <option value="<?= $author['id'] ?>">
+              <?= $author['lastName'] . ' ' . $author['firstName'] ?>
+            </option>
+          <?php } ?>
+        </select>
+      </div>
+      <div class="mb-3 col">
+        <label for="century" class="form-label">Siècle :</label>
+        <select name="century" id="century" class="form-select">
+          <option selected value></option>
+          <?php foreach ($centuries as $century) { ?>
+            <option value="<?= $century[0] ?>">
+              <?= $century[0] ?>
+            </option>
+          <?php } ?>
+        </select>
+      </div>
     </div>
-    <div class="mb-3">
-      <label for="century" class="form-label">Siècle :</label>
-      <select name="century" id="century">
-        <option disabled selected value></option>
-        <?php foreach ($centuries as $century) { ?>
-          <option value="<?= $century[0] ?>">
-            <?= $century[0] ?>
-          </option>
-        <?php } ?>
-      </select>
+    <div class="row">
+      <div class="mb-3 form-check col ms-2">
+        <label for="author" class=" form-check-label">Trier par auteur</label>
+        <input type="radio" name="sortBy" value="author" class="form-check-input">
+      </div>
+      <div class="mb-3 form-check col ms-2">
+        <label for="century" class=" form-check-label">Trier par siècle</label>
+        <input type="radio" name="sortBy" value="century" class="form-check-input">
+      </div>
     </div>
-    <div class="mb-3 form-check">
-      <label for="author" class=" form-check-label">Trier par auteur</label>
-      <input type="radio" name="sortBy" value="author" class="form-check-input">
-    </div>
-    <div class="mb-3 form-check">
-      <label for="century" class=" form-check-label">Trier par siècle</label>
-      <input type="radio" name="sortBy" value="century" class="form-check-input">
-    </div>
-    <input type="submit" value="Rechercher">
+    <input type="submit" value="Rechercher" class="btn btn-primary mb-3">
   </form>
 </div>
 <div class="container">
   <nav>
-    <ul>
-      <li><a href="saisiecit.php">Ajouter une citation</a></li>
-    </ul>
+    <div class="list-group">
+      <a href="saisiecit.php" class="list-group-item list-group-item-action">Ajouter une citation</a>
+          </div>
   </nav>
 </div>
